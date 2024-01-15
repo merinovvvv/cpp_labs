2f2cc6f1ad14be1f2b20fb8cb1335546abc8a87f
Yaraslau Merynau <yar.merinov@mail.ru>
2024-01-15 16:46:51 +0300

    seminar14.12

--- seminar14.12/vehicle/ElectricCar.cpp
+++ seminar14.12/vehicle/ElectricCar.cpp
@@ -1,18 +1,18 @@
 #include "ElectricCar.h"
 
-ElectricCar::ElectricCar(int maxSpeed, double engineCapacity, EngineType engineType, int seatsNumber, int powerReserve) : Vehicle(maxSpeed, engineCapacity, engineType), Car(maxSpeed, engineCapacity, engineType, seatsNumber), ElectricVehicle(maxSpeed, engineCapacity, engineType, powerReserve) {
+ElectricCar::ElectricCar(int maxSpeed, double engineCapacity, EngineType engineType, int seatsNumber, int powerReserve) : Car(maxSpeed, engineCapacity, engineType, seatsNumber), ElectricVehicle(maxSpeed, engineCapacity, engineType, powerReserve) {
 	spdlog::debug("ElectricCar costr with parametres: {0:d}, {1:f}, {2:s}, {3:d}, {4:d}", maxSpeed, engineCapacity, engineTypeToString(engineType), seatsNumber, powerReserve);
 }
 ElectricCar::~ElectricCar() {
 	spdlog::debug("ElectricCar's destr");
 }
 void ElectricCar::engineOn() const{
-	spdlog::info("Electric car's engine is on");
+	spdlog::info("Electric car's engine is on");	
 }
 void ElectricCar::engineOff() const{
 	spdlog::info("Electric car's engine is off");
 }
 std::string ElectricCar::toString() const{
 	spdlog::debug("Electic car's toString func");
-	return Vehicle::toString() + " Seats number: " + std::to_string(seatsNumber_) + " Power reserve: " + std::to_string(powerReserve_);
+	return Car::toString() + " Power reserve: " + std::to_string(powerReserve_);
 }
